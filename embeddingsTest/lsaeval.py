6532e17bba8c666fd0d9d342e40ef50312b68f23
Atish Jain <jainm2@hugegraph-1.mark-feblowitz-s-account.cloud>
2020-02-17 17:24:02 -0600

    lsa backup changes after merging with Jakob's to integrate

---
+++ embeddingsTest/lsaeval.py
@@ -0,0 +1,27 @@
+import nltk 
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+import json
+from sklearn.decomposition import TruncatedSVD
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+from nltk.corpus import stopwords
+wordnet_lemmatizer = WordNetLemmatizer()
+file=open('scipy.linalg.det.json')
+
+with open('scipy.linalg.det.json') as f:
+  data = json.load(f)
+listData=data['stackoverflow']
+contentCorpus=""
+for i in listData:
+    contentCorpus=contentCorpus+i['_source']['content']
+projectStopwords=stopwords.words('english')
+additionalStopWords=['<','>','/p','.']
+for i in additionalStopWords:
+    projectStopwords.append(i)
+tokens = nltk.tokenize.word_tokenize(contentCorpus)
+tokens = [wordnet_lemmatizer.lemmatize(t) for t in tokens] 
+tokens = [t for t in tokens if t not in projectStopwords]
+print(tokens)
+
