e5055f9b4669fa5544b91a15b9179273d2d2e726
Atish Jain <jainm2@hugegraph-1.mark-feblowitz-s-account.cloud>
2020-02-25 19:12:29 -0600

    tnse with pickling, integrated with jAKOB'S CODe added some exception handling,

--- embeddingsTest/lsaeval.py
+++ embeddingsTest/lsaeval.py
@@ -1,11 +1,13 @@
-import nltk 
+import nltk
+import pickle
+import os 
 import numpy as np
 from nltk.stem import WordNetLemmatizer
 import json
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-from testscript import clean_text
+from soupclean import clean_text
 from sklearn.decomposition import TruncatedSVD
 #nltk.download('stopwords')
 #nltk.download('punkt')
@@ -28,45 +30,50 @@ from sklearn.decomposition import TruncatedSVD
 #tokens = [wordnet_lemmatizer.lemmatize(t) for t in tokens] 
 #tokens = [t for t in tokens if t not in projectStopwords]
 #print(tokens)
-files=['numpy.linalg.linalg.inv.json','scipy.linalg.det.json','sklearn.linear_model.LinearRegression.json','sklearn.linear_model.logistic.softmax.json',
-'sklearn.metrics.regression.mean_squared_error.json',
-'sklearn.metrics.scorer.f1_score.json']
+jsonFileDir="../../../../data/data/datascience_stackexchange_graph_v2/all"
+files=[]
+for i in os.listdir(jsonFileDir):
+    files.append(jsonFileDir+"/"+i)
+    
+#files=['numpy.linalg.linalg.inv.json','scipy.linalg.det.json','sklearn.linear_model.LinearRegression.json','sklearn.linear_model.logistic.softmax.json',
+#'sklearn.metrics.regression.mean_squared_error.json',
+#'sklearn.metrics.scorer.f1_score.json']
 wordArr=[]
 moduleArr=[]
 lookupDict={}
 lookdownDict={}
 currentRow=0
+j=0
+interested_file_len=100
 for i in files:
    moduleArr.append(i)
-   words=clean_text(i)
+   if j == interested_file_len:
+       break
+
+   #print("this file is causing an error",i)
+   try:
+       words=clean_text(i)
+   except:
+       pass
    wordArr.append(words)
    for word in words:
        if word not in lookupDict:
           lookupDict[word]=currentRow
           currentRow=currentRow+1
           lookdownDict[currentRow-1]=word
-
+   j=j+1
 #X=np.zeros((currentRow,len(files)))
 ##documentrelevance
-X=np.zeros((len(files),currentRow))
+X=np.zeros((interested_file_len,currentRow))
 
 for j in range(len(wordArr)):
     for word in wordArr[j]:
        # X[lookupDict[word],j]=X[lookupDict[word],j]+1
        ##document relevance
        X[j,lookupDict[word]]=X[j,lookupDict[word]]+1
+pickle.dump( X, open( "save.x", "wb" ) )
+pickle.dump(moduleArr,open("save.moduleArr","wb"))
 #svd=TruncatedSVD(n_components=1)
 #Z=svd.fit_transform(X)
 #for i in range(len(Z)):
 #    print(Z[i],lookdownDict[i])
-       
-svd = TruncatedSVD()
-Z = svd.fit_transform(X)
-plt.scatter(Z[:,0], Z[:,1])
-for i in range(len(Z)):
-    ##plt.annotate(s=lookdownDict[i], xy=(Z[i,0], Z[i,1]))
-    ##documentrelevance
-    plt.annotate(s=moduleArr[i], xy=(Z[i,0], Z[i,1]))
-plt.show()
-out_png = 'lsaspacialembeddingdocuments.png'
-plt.savefig(out_png, dpi=150)
---
+++ embeddingsTest/lsaevaltnse.py
@@ -0,0 +1,40 @@
+
+import json
+from sklearn.decomposition import TruncatedSVD
+#import pandas as pd
+from sklearn.manifold import TSNE
+
+#%matplotlib inline
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import seaborn as sns
+import pickle
+import time 
+X=pickle.load( open( "save.x", "rb" ) )
+moduleArr=pickle.load(open("save.moduleArr","rb"))
+
+svd = TruncatedSVD(n_components=4, n_iter=7, random_state=42)
+Z = svd.fit_transform(X)
+
+time_start = time.time()
+tsne = TSNE(n_components=2, verbose=1, perplexity=4, n_iter=300)
+tsne_results = tsne.fit_transform(Z)
+#df_subset['tsne-2d-one'] = tsne_results[:,0]
+#df_subset['tsne-2d-two'] = tsne_results[:,1]
+plt.figure(figsize=(16,10))
+for i in range(len(moduleArr)):
+    plt.scatter(tsne_results[:,0],tsne_results[:,1],label=moduleArr[i])
+print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+out_png = 'lsaspacialemTNSE.png'
+plt.savefig(out_png, dpi=150)
+
+#print("pickle loaded")
+#svd = TruncatedSVD()
+#Z = svd.fit_transform(X)
+#plt.scatter(Z[:,0], Z[:,1])
+#for i in range(len(Z)):
+#        plt.annotate(s=moduleArr[i], xy=(Z[i,0], Z[i,1]))
+#        plt.show()
+#        out_png = 'lsaspacialembeddingdocuments.png'
+#        plt.savefig(out_png, dpi=150)
+
