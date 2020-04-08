ac1aa1b7d48f5f39787b4baec8e4a062f17abd2e
Jakob Didio <didioj2@rpi.edu>
2020-04-08 17:41:44 -0500

    Added files for having run Doc2Vec on all modules.

--- embeddingsTest/lsaMatrix.py
+++ embeddingsTest/lsaMatrix.py
@@ -7,11 +7,23 @@ def computeMatrix(mrOutput):
 	docList = set()
 	wordList = []
 	docAndWordDict = {}
+	totalEntries = 0
 	for line in inputFile:
+		totalEntries += 1
+	currentEntry = 1
+	inputFile.close()
+	inputFile = open(mrOutput, 'r')
+	for line in inputFile:
+		'''if currentEntry%50 == 0:
+			print("Parsing entry", currentEntry, "out of", totalEntries)'''
 		searchedLine = re.search('\["(.+)", "(.+\.json)"\]\t(.+)', line)
-		wordName = searchedLine.group(1)
-		docName = searchedLine.group(2)
-		num = searchedLine.group(3)
+		try:
+			wordName = searchedLine.group(1)
+			docName = searchedLine.group(2)
+			num = searchedLine.group(3)	
+		except:
+			print("This line is not parseable by the regex", line)
+			continue
 		trueNum = float(num)
 		docList.add(docName)
 		if wordName not in wordList:
@@ -21,16 +33,27 @@ def computeMatrix(mrOutput):
 			docAndWordDict[docName][wordName] = trueNum
 		else:
 			docAndWordDict[docName][wordName] = trueNum
+		currentEntry += 1
 
 	wholeMatrix = []
+	totalDocs = 0
 	for document in docList:
+		totalDocs += 1
+	currentDoc = 1
+	for document in docList:
+		'''if currentDoc%5 == 0:
+			print("Going through column for document", currentDoc, "out of", totalDocs)'''
 		data = []
 		for word in wordList:
-			if word in docAndWordDict[document]:
+			if word in docAndWordDict[document]:	
 				data.append(docAndWordDict[document][word])
-			else:
+			else:	
 				data.append(0)
 		wholeMatrix.append(data)
+		currentDoc += 1
 	finalMatrix = np.array(wholeMatrix)
 	inputFile.close()
 	return finalMatrix
+
+if __name__ == "__main__":
+	testMatrix = computeMatrix("output_try2")
--- embeddingsTest/testFlow.py
+++ embeddingsTest/testFlow.py
@@ -1,13 +1,15 @@
-import shortParseFiles as sp
+import fivehrParallelParseFiles as sp
 import pickleFiles as pf
 import os.path as osPath
 
-if osPath.isfile("smallPickledFiles.p"):
+if osPath.isfile("allPickledFiles.p"):
 	print("Found pickled data.")
-	dataTuple = pf.load_text("smallPickledFiles.p")
+	dataTuple = pf.load_text("allPickledFiles.p")
 	print(dataTuple[1][0])
+	print(len(dataTuple[0]))
+	print(len(dataTuple[1]))
 else:
 	print("No pickled data, parsing text.")
 	dataTuple = sp.parse_text()
-	pf.store_text(dataTuple, "smallPickledFiles.p")
+	pf.store_text(dataTuple, "allPickledFiles.p")
 	print(dataTuple[1][0])
