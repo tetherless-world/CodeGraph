6527cc5dd67c12ac565072802cf9417dbaa7d0dd
Jakob Didio <didioj2@rpi.edu>
2020-04-10 15:49:32 -0500

    Computed within training set similarities.

--- embeddingsTest/computeSimilarity.py
+++ embeddingsTest/computeSimilarity.py
@@ -1,7 +1,13 @@
 import unrelatedParseFiles as up
 from gensim.models.doc2vec import Doc2Vec
+import os
 
 if __name__ == "__main__":
+	jsonFileDir = '/data/data/datascience_stackexchange_graph_v2/all/'
+	files = [i for i in os.listdir(jsonFileDir) if i != 'first500']
 	model = Doc2Vec.load("stressTestOutput.model")
-	similar_doc = model.docvecs.most_similar('tensorflow.python.add.json','', 10)
-	print(similar_doc)
+	for file in files:
+		print("Documents most similar to", file, "are:")
+		similarities = model.docvecs.most_similar(file, '', 10)
+		for i in range(0, 5):
+			print('\t', str(i+1) + '.', similarities[i][0])
