f37e63378ee2e106185e5243ee79a104b9eb5c2a
Atish Jain <jainm2@hugegraph-1.mark-feblowitz-s-account.cloud>
2020-05-04 12:20:11 -0500

    workflow integration done

--- embeddingsTest/allStressTest.py
+++ embeddingsTest/allStressTest.py
@@ -2,7 +2,7 @@ import fivehrParallelParseFiles as sp
 import pickleFiles as pf
 import os.path as op
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-numofEpochs=300
+numofEpochs=130
 ##tune this later
 if op.isfile('allPickledFiles.p'):
         print("Pickled data detected")
@@ -21,7 +21,7 @@ else:
         data = []
         for i in range(0, len(dataTuple[0])):
                 data.append(TaggedDocument(dataTuple[0][i], [dataTuple[1][i]]))
-        model = Doc2Vec(vector_size=600,alpha=0.01,min_alpha=0.0001,min_count=3,dm=1, workers=96)
+        model = Doc2Vec(vector_size=1300,alpha=0.01,min_alpha=0.0001,min_count=1,dm=1, workers=96)
         model.build_vocab(data)
         model.train(data,total_examples=model.corpus_count,epochs=numofEpochs)
         model.save('newStressTestOutput.model')
--- embeddingsTest/docstringModelTrain.py
+++ embeddingsTest/docstringModelTrain.py
@@ -1,23 +1,54 @@
-import fivehrParallelParseFiles as sp
 import pickleFiles as pf
 import os.path as op
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-numofEpochs=300
+
 ##tune this later
-if op.isfile('cleanedDocStringText.p'):
+hyperparameters=[{"numofEpochs":30,"vector_size":300,"min_count":5},{"numofEpochs":30,"vector_size":600,"min_count":5},{"numofEpochs":30,"vector_size":100,"min_count":5},{"numofEpochs":30,"vector_size":300,"min_count":1},{"numofEpochs":30,"vector_size":300,"min_count":10}]
+for params in hyperparameters:
+      if op.isfile('cleanedDocStringText.p'):
+              print("Pickled data detected")
+              dataList = pf.load_text("cleanedDocStringText.p")
+              data = []
+              print('Text loaded from pickled data')
+              for i in range(0, len(dataList)):
+                      data.append(TaggedDocument(dataList[i][1], [dataList[i][0]]))
+              print('List of data documents created.')
+              model = Doc2Vec(vector_size=params['vector_size'],alpha=0.01,min_alpha=0.0001,min_count=params['min_count'],dm=1, workers=96)
+              print('Model initialized.')
+              model.build_vocab(data)
+              print('Vocab initialized. Training beginning.')
+              model.train(data,total_examples=model.corpus_count,epochs=params['numofEpochs'])
+              model.save('docstringStressTestOutput.model')
+              print("Model saved")
+      else:   
+              print("No pickled data detected.")
+                                            
+      
+      
+      model = Doc2Vec.load("docstringStressTestOutput.model")
+      if op.isfile('cleanedDocStringText.p'):
         print("Pickled data detected")
-        dataList = pf.load_text("cleanedDocStringText.p")
-        data = []
-        print('Text loaded from pickled data')
-        for i in range(0, len(dataList)):
-                data.append(TaggedDocument(dataList[i][1], [dataList[i][0]]))
-        print('List of data documents created.')
-        model = Doc2Vec(vector_size=300,alpha=0.01,min_alpha=0.0001,min_count=2,dm=1, workers=48)
-        print('Model initialized.')
-        model.build_vocab(data)
-        print('Vocab initialized. Training beginning.')
-        model.train(data,total_examples=model.corpus_count,epochs=numofEpochs)
-        model.save('docstringStressTestOutput.model')
-        print("Model saved")
-else:   
-        print("No pickled data detected.")
+        dataTuple = pf.load_text("cleanedDocStringText.p")
+
+        #print("Embeddings for:",file=open("output_new/embeddings_output.txt", "a"))
+        ##uncomment for vector outputs
+
+        #for i in range(len(dataTuple)):
+
+            #print("inferred vecto for")
+          #  print(dataTuple[i][0],file=open("output_new/embeddings_output.txt", "a") )
+            # inferred_vector = model.infer_vector(dataTuple[i][1])
+            #print(inferred_vector,file=open("output_new/embeddings_output.txt", "a"))
+        accuracy=0
+        print("number of docs",len(dataTuple))
+        iters=100
+        for i in range(iters):
+                inferred_vector = model.infer_vector(dataTuple[i][1])
+                most_similar= model.docvecs.most_similar([inferred_vector],topn=1)
+                if most_similar[0][0] == dataTuple[i][0]:
+                    accuracy=accuracy+1
+                else:
+                  pass
+                  # # print("Files not accurately embedded",dataTuple[i][0],file=open("output_new/embeddings_wrongly_done.txt","a"))
+                  # print("\n predicted instead",most_similar[0][0],file=open("output_new/embeddings_wrongly_done.txt","a"))
+        print("Training accuracy for embeddings using Doc2vec with hyperparameters vector_size="+str(params['vector_size'])+"min_count="+str(params['min_count'])+"numofEpochs"+str(params['numofEpochs']),accuracy/iters,file=open("output_new/embeddings_accuracy.txt","a"))
