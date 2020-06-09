a80d6c7d4ba32a5b742a0dd808e7e2b6f17b148a
Atish Jain <atishjainms@gmail.com>
2020-06-09 11:25:49 -0400

    mappings

--- embeddingsTest/parseUSECSV.py
+++ embeddingsTest/parseUSECSV.py
@@ -1,10 +1,21 @@
 if __name__ == '__main__':
-	embeddingspath = '../../data/embeddings.csv'
-	with open(embeddingspath, 'r') as embeddings:
-		i = 0
-		for line in embeddings:
-			if i == 1000000:
-				break
-			newline = line.rstrip()
-			parsedline = newline.split(',')
-			i += 1
+        embeddingspath = '../../data/codeGraph/embeddings.csv'
+        embeddingsToLabelPath='../../data/codeGraph/embeddingtolabel.csv'
+        textToLabelPath='../../data/codeGraph/labeltotext.csv'
+        with open(embeddingspath, 'r') as embeddings, open(textToLabelPath,'r') as embeddingsToLabels,open(embeddingsToLabelPath,'r') as textToLabels:
+                i = 0
+                for (line1,line2,line3) in zip(embeddings,embeddingsToLabels,textToLabels):
+                        if i == 2:
+                                break
+                        newline = line1.rstrip()
+                        parsedline = newline.split(',')
+                        print(parsedline)
+                        newline = line2.rstrip()
+                        parsedline = newline.split(',')
+                        print(parsedline)
+
+                        newline = line3.rstrip()
+                        parsedline = newline.split(',')
+                        print(parsedline)
+                        i += 1
+
