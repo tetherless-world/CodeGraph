df37ac8bf06a714ae7d634946a568ac7ad4782dc
Atish Jain <atishjainms@gmail.com>
2020-06-11 11:43:15 -0400

    for evaluations

---
+++ embeddingsTest/storeIndicesForComparison.py
@@ -0,0 +1,68 @@
+import faiss 
+import csv
+import numpy as np
+import sys
+import pickle
+if __name__ == '__main__':
+        embeddingspath = '../../data/codeGraph/embeddings.csv'
+        embeddingsToLabelPath='../../data/codeGraph/embeddingtolabel.csv'
+        textToLabelPath='../../data/codeGraph/labeltotext.csv'
+        embeded_docmessages=[]
+        index = faiss.IndexFlatL2(512)
+
+        with open(embeddingspath, 'r') as embeddings, open(embeddingsToLabelPath,'r') as embeddingsToLabels,open(textToLabelPath,'r') as textToLabels:
+                embeddingtolabelmap = {}
+                labeltotextmap = {}
+                for (line1,line2,line3) in zip(embeddings,embeddingsToLabels,textToLabels):
+
+                        newline = line1.rstrip()
+                        parsedline = newline.split(',')
+                        embeded_docmessages.append(parsedline)
+                        linetoadd = np.asarray(parsedline, dtype=np.float32).reshape(1,-1)
+                        index.add(linetoadd)
+                        #print(parsedline)
+                        newline = line2.rstrip()
+                        parsedline = newline.split(',')
+                        splitembedding = parsedline[0].split(';')
+                        arrayembedding = np.asarray(splitembedding, dtype=np.float32).reshape(1,-1)
+                        arrayembedding = arrayembedding.tolist()
+                        #print(arrayembedding)
+                        finalembedding = tuple(arrayembedding[0])
+                        #print(finalembedding)
+                        embeddingtolabelmap[finalembedding] = parsedline[1]
+                        #print(parsedline)
+
+                        newline = line3.rstrip()
+                        parsedline = newline.split(',')
+                        try:
+                            labeltotextmap[parsedline[0]] = parsedline[1]    
+                        # this was originally included to find a bug caused by not omitting
+                        # carriage return in the csv, feel free to leave this part out
+                        except IndexError:
+                            print(newline)
+                            print('\n\n\n\n\nSEPARATOR\n\n\n\n\n')
+                            print(parsedline)
+                            exit()
+                        #print(parsedline)
+                        
+                        
+#                        i += 1
+                k=11
+                    
+                embeded_distance_index_info=[]
+                embeded_distance_info=[]
+                embeded_docmessages=np.asarray(embeded_docmessages,dtype=np.float32)
+                with open('embeded_docmessages.pickle', 'wb') as f:
+                    pickle.dump(embeded_docmessages, f, pickle.HIGHEST_PROTOCOL)
+                faiss.write_index(index, 'faiss_index.saved')
+                with open('labeltotextmap.pickle', 'wb') as f:
+                    pickle.dump(labeltotextmap, f, pickle.HIGHEST_PROTOCOL)
+                with open('embeddingtolabelmap.pickle', 'wb') as f:
+                    pickle.dump(embeddingtolabelmap, f, pickle.HIGHEST_PROTOCOL)
+                print("Program terminated")
+
+    
+
+
+
+
