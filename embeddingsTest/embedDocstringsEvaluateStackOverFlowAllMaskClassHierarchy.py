58bb7f9239f981a9553e642a34ad6eeb78914a3c
Atish Jain <atishjainms@gmail.com>
2020-08-07 09:47:46 -0400

    Class heirarchy

--- embeddingsTest/embedDocstringsEvaluateStackOverFlowAllMaskClassHierarchy.py
+++ embeddingsTest/embedDocstringsEvaluateStackOverFlowAllMaskClassHierarchy.py
@@ -1,4 +1,3 @@
-
 import ijson
 import tensorflow_hub as hub
 import faiss
@@ -10,21 +9,10 @@ from statistics import mean
 from statistics import pstdev
 import nltk
 nltk.download('punkt')
-from sentence_transformers import SentenceTransformer
-
 from nltk.tokenize import sent_tokenize
-
-def run_differentmodels():
-        modelList = ['bert-base-nli-stsb-mean-tokens', 'bert-large-nli-stsb-mean-tokens','roberta-base-nli-stsb-mean-tokens','roberta-large-nli-stsb-mean-tokens', 'distilbert-base-nli-stsb-mean-tokens']
-        for i in modelList:
-                print("running model",i)
-                dataTuple = build_index(i)
-                evaluate_neighbors(dataTuple[0], dataTuple[1], dataTuple[2], dataTuple[3],dataTuple[4],dataTuple[5],i)
-                
-    
-def build_index(model):
+def build_index():
     embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
-
+    index = faiss.IndexFlatL2(512)
     docMessages = []
     embeddingtolabelmap = {}
     labeltotextmap = {}
@@ -33,16 +21,9 @@ def build_index(model):
     droppedClassWithLessLength=set()
     docStringLength_avg=[]
     docLabelToTextForSentenceTokenizationAndAnalysis= {}
-
     with open('../../data/codeGraph/stackoverflow_questions_per_class_func_3M_filtered_new.json', 'r') as data,open('./lengthAnalysisDocstrings_NewJson.txt', 'w') as outputFile:
         jsonCollect = ijson.items(data, 'results.bindings.item')
         i = 0
-        transformer = SentenceTransformer(model)
-        embeddedDocTextLen = len(transformer.encode(["index builder"])[0])
-        index = faiss.IndexFlatL2(embeddedDocTextLen)
-        originalout = sys.stdout
-        sys.stdout = outputFile
-
         for jsonObject in jsonCollect:
             objectType = jsonObject['class_func_type']['value'].replace(
                 'http://purl.org/twc/graph4code/ontology/', '')
@@ -55,55 +36,48 @@ def build_index(model):
             for code in soup.find_all('code'):
                 code.decompose()
             docStringText = soup.get_text()
-            
             if docStringText in embedCollect:
-                ##had to include this because the same class was getting addded into the mapped array
                 if docLabel in duplicateClassDocString:
                     pass
                     
                 else:
                     if len(docStringText) < -1:
-                        print("has less than -1 character,class:",docLabel,"docstring:",docStringText)
                         droppedClassWithLessLength.add(docLabel)
                         continue
                     duplicateClassDocString.add(docLabel)
                     docStringLength_avg.append(len(docStringText))
-                    print("doclabel",docLabel)
+
                     docLabelToTextForSentenceTokenizationAndAnalysis[docLabel]=docStringText
-                    embeddedDocText = transformer.encode([docStringText])
-                    print("embeddedDoctext shape",np.asarray(embeddedDocText, dtype=np.float32).shape)
-
-                    embeddingtolabelmap[np.asarray(embeddedDocText, dtype=np.float32).tobytes()] = [docLabel]
-
+                    embeddedDocText = embed([docStringText])[0]
+                    embeddingtolabelmap[tuple(
+                    embeddedDocText.numpy().tolist())].append(docLabel)
             else:
                 if len(docStringText) < -1:
-                        print("has less than -1 character,class:",docLabel,"docstring:",docStringText)
                         droppedClassWithLessLength.add(docLabel)
                         continue
                 duplicateClassDocString.add(docLabel)
                 embedCollect.add(docStringText)
                 docStringLength_avg.append(len(docStringText))
                 docLabelToTextForSentenceTokenizationAndAnalysis[docLabel]=docStringText
-                print("doclabel",docLabel)
-                print(len(docStringText))
-                
-                embeddedDocText = transformer.encode([docStringText])  
-                print("embeddedDoctext shape",np.asarray(embeddedDocText, dtype=np.float32).shape)
-                docMessages.append(np.asarray(embeddedDocText, dtype=np.float32))
-                index.add(np.asarray(embeddedDocText, dtype=np.float32))
-                embeddingtolabelmap[np.asarray(embeddedDocText, dtype=np.float32).tobytes()] = [docLabel]
+
+                embeddedDocText = embed([docStringText])[0]
+                newText = np.asarray(
+                embeddedDocText, dtype=np.float32).reshape(1, -1)
+                docMessages.append(embeddedDocText.numpy().tolist())
+                index.add(newText)
+                embeddingtolabelmap[tuple(
+                embeddedDocText.numpy().tolist())] = [docLabel]
 #                 if  docLabel == 'pysnmp.smi.rfc1902.ObjectType':
 #                     print("text for pysnmp.smi.rfc1902.ObjectType' is")
 #                     print(docStringText)
 #            labeltotextmap[docLabel] = docStringText
             i += 1
-        sys.stdout=originalout
 
         return (index, docMessages, embeddingtolabelmap, docStringLength_avg,droppedClassWithLessLength,docLabelToTextForSentenceTokenizationAndAnalysis)
 
 
-def evaluate_neighbors(index, docMessages, embeddingtolabelmap,docStringLength_avg,droppedClassWithLessLength,docLabelToTextForSentenceTokenizationAndAnalysis,model):
-    k = 3
+def evaluate_neighbors(index, docMessages, embeddingtolabelmap,docStringLength_avg,droppedClassWithLessLength,docLabelToTextForSentenceTokenizationAndAnalysis):
+    k = 10
     fp=0
     fn=0
     tp=0
@@ -116,10 +90,18 @@ def evaluate_neighbors(index, docMessages, embeddingtolabelmap,docStringLength_a
     exactpositivepresent=False
     totaldocs=0
     embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
-    originalout = sys.stdout
-    transformer = SentenceTransformer(model)
-    with open('../../data/codeGraph/stackoverflow_questions_per_class_func_3M_filtered_new.json', 'r') as data, open('./stackNewJsonAllMask_'+model+'_.txt', 'w') as outputFile:
 
+    with open('../../data/codeGraph/stackoverflow_questions_per_class_func_3M_filtered_new.json', 'r') as data, open('../../data/codeGraph/classes2superclass.out', 'r') as class2superclass, open('../../data/codeGraph/classes.map', 'r') as classes,open('lengthAnalysisStackOverflowAllMask.txt', 'w') as outputFile:
+        getHierarchy = ijson.items(class2superclass, 'results.bindings.item')
+        classToSuperClass={}
+
+        for iterateInHierarchy in getHierarchy:
+            superClass = iterateInHierarchy['superclass']['value'].replace('http://purl.org/twc/graph4code/python/','')
+            class_sub = iterateInHierarchy['class']['value'].replace('http://purl.org/twc/graph4code/python/','')
+            classToSuperClass[class_sub]=superClass
+          
+
+           
         firstJsonCollect = ijson.items(data, 'results.bindings.item') 
         postMap = {}
         for jsonObject in firstJsonCollect:
@@ -137,10 +119,11 @@ def evaluate_neighbors(index, docMessages, embeddingtolabelmap,docStringLength_a
                 postMap[stackText].append(classLabel) 
             else:
                 postMap[stackText] = [classLabel]
+        correctHierarchy=0
+        wrongHierarchy=0
         data.close()
         newData = open('../../data/codeGraph/stackoverflow_questions_per_class_func_3M_filtered_new.json', 'r')
         jsonCollect = ijson.items(newData, 'results.bindings.item')
-        sys.stdout = outputFile
         for jsonObject in jsonCollect:
             totaldocs+=1
             objectType = jsonObject['class_func_type']['value'].replace(
@@ -159,9 +142,6 @@ def evaluate_neighbors(index, docMessages, embeddingtolabelmap,docStringLength_a
             stackText = soup.get_text()
             if len(stackText) < 50:
                 continue
-#              print('\nTitle of Stack Overflow Post:', title)
-            print('Class associated with post:', classLabel, '\n')
-            print('Text of post before masking:', stackText, '\n')
             maskedText = None
             for foundLabel in postMap[stackText]: 
                 splitLabel = foundLabel.lower().split('.')
@@ -171,60 +151,44 @@ def evaluate_neighbors(index, docMessages, embeddingtolabelmap,docStringLength_a
                     partPattern = re.compile(labelPart, re.IGNORECASE)
                     maskedText = partPattern.sub(' ', maskedText)#maskedText.replace(labelPart, ' ')
 
-            embeddedText = transformer.encode([maskedText]) #maskedText
-            D, I = index.search(  np.asarray(embeddedText, dtype=np.float32), k)
+## masking removed for now
+
+            embeddedText = embed([maskedText])#[stackText])
+            embeddingVector = embeddedText[0]
+            embeddingArray = np.asarray(
+                embeddingVector, dtype=np.float32).reshape(1, -1)
+            D, I = index.search(embeddingArray, k)
             distances = D[0]
             indices = I[0]
 #             print("Distances of related vectors:", distances)
 #             print("Indices of related vectors:", indices)
             positivepresent=False
             exactpositivepresent=False
+
             for p in range(0, k):
                 properIndex = indices[p]
                 embedding = docMessages[properIndex]
-                adjustedembedding = np.asarray(embedding, dtype=np.float32).tobytes()
+                adjustedembedding = tuple(embedding)
                 label = embeddingtolabelmap[adjustedembedding]
                 ##multiple docstrings associated with the same embedding mapped
                 ##array of labels mapped
                 j=0
                 for l in label:
-                    
-                    if l.startswith(classLabel.split(".")[0]):
-                        positivepresent=True
-                        if j == 0:
-                             print("\n True positive label being contributed by \n",l)
-                        else:
-                            print("and \t",l)
+                    if classLabel not in classToSuperClass or l not in classToSuperClass:
+                        continue
+           ##not all labels present
+                    if classToSuperClass[l]==classToSuperClass[classLabel]:
+                        correctHierarchy=correctHierarchy+1
                     else:
-                        print("class not associated",l)
-                    if l == classLabel:
-                        exactpositivepresent=True
-                        print("\n Exact positive label being contributed by \n",l)
-                    j=j+1
+                        wrongHierarchy=wrongHierarchy+1
                         
-            if not positivepresent:
-                fp=fp+1
-                print("Loose False Positive Present \n")
-                print("Investigating the reason with sentence tokenized docstring for:", classLabel,"\n")
-                print(sent_tokenize(docLabelToTextForSentenceTokenizationAndAnalysis[classLabel]))
-            else:
-                tp=tp+1
-#                 print("Loose True Positive Present -------------------------------------------------------- \n")
-            if not exactpositivepresent:
-                efp=efp+1
-#                 print("match  False Positive Present ------------------------------------------------------- \n")
-            else:
-                etp=etp+1
-            
-#                 print("match True Positive Present -------------------------------------------------------- \n")
-        print("--------------------------------------------- \n")
-        
+        print("iterms correctly classified",correctHierarchy)
+        print("iterms wrongly classfied",wrongHierarchy)
 
-        print(tp/(tp+fp), " Loose Precision at 5 with all masking for model: "+model)
-        print(etp/(etp+efp), "Exact Precision at 5 with all  masking for model: "+model)
+        print("hierarchy accuracy %=",correctHierarchy*100/(correctHierarchy+wrongHierarchy))
 
-        sys.stdout=originalout
 
 if __name__ == '__main__':
-    run_differentmodels()
-        
+    dataTuple = build_index()
+    print("Completed building index.")
+    evaluate_neighbors(dataTuple[0], dataTuple[1], dataTuple[2], dataTuple[3],dataTuple[4],dataTuple[5])
