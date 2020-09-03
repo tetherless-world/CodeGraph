0e7bb61cd01264a19a0bbd4e34dc5099aa0962b7
Julian Dolby <dolby@codebreaker.sl.cloud9.ibm.com>
2020-09-03 17:43:24 +0000

    usage with mrr

---
+++ embeddingsTest/tasks/usageAnalysis/staticAnalysis2.py
@@ -0,0 +1,244 @@
+
+import ijson
+import tensorflow_hub as hub
+import faiss
+import numpy as np
+from bs4 import BeautifulSoup
+import sys
+import re
+import sys
+from metrics_eval import ranking_metrics
+
+def build_class_mapping(mapPath):
+    classMap = {}
+    with open(mapPath, 'r') as inputFile:
+        for line in inputFile:
+            lineComponents = line.rstrip().split(' ')
+            if len(lineComponents) < 2:
+                classMap[lineComponents[0]] = lineComponents[0]
+            else:
+                classMap[lineComponents[0]] = lineComponents[1]
+    return classMap
+
+def build_index_docs(docPath):
+    embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
+    index = faiss.IndexFlatIP(512)
+    classesToDocs = {}
+    docsToClasses = {}
+    embedList = {}
+    
+    with open(docPath, 'r') as data:
+        jsonCollect = ijson.items(data, 'item')
+        i = 0
+        for jsonObject in jsonCollect:
+            if 'class_docstring' not in jsonObject:
+                continue
+            className = jsonObject['klass']
+            docStringText = jsonObject['class_docstring']
+
+            soup = BeautifulSoup(docStringText, 'html.parser')
+            for code in soup.find_all('code'):
+                code.decompose() # this whole block might be unnecessary
+            docStringText = soup.get_text()
+
+            if docStringText in docsToClasses:
+                docClasses = docsToClasses[ docStringText ]
+                
+                if className in docClasses:
+                    pass
+                    
+                else:
+                    docClasses.append(className)
+
+            else:
+                docsToClasses[ docStringText ] = [ className ]
+
+            classesToDocs[ className ] = docStringText
+            
+    docList = np.array(list(docsToClasses.keys()))
+    embeddedDocText = np.array(embed(docList))
+    faiss.normalize_L2(embeddedDocText)
+    index.add(embeddedDocText)
+    
+    return (index, docList, docsToClasses, embeddedDocText, classesToDocs)
+
+
+def build_static_map(usagePath, docsToClasses):
+    staticMap = {}
+    with open(usagePath, 'r') as staticData:
+        matchString = '(.+) (\d+) \[(.+)\]'
+        for line in staticData:
+            pattern = re.compile(matchString)
+            adjustedLine = pattern.match(line)
+            if adjustedLine == None:
+                print("Found violation.")
+                print(line)
+            separateClasses = adjustedLine.group(3).strip().split(', ')
+            staticMap[adjustedLine.group(1)] = separateClasses
+    return staticMap
+
+def build_sibling_maps(hierarchyPath):
+    childToParentMap = {}
+    parentToChildMap = {}
+    with open(hierarchyPath, 'r') as data:
+        jsonCollect = ijson.items(data, 'results.bindings.item')
+        for jsonObject in jsonCollect:
+            child = jsonObject['class']['value']
+            parent = jsonObject['superclass']['value'].replace('http://purl.org/twc/graph4code/python/', '')
+            if child in childToParentMap:
+                childToParentMap[child].append(parent)
+            else:
+                childToParentMap[child] = [parent]
+            if parent in parentToChildMap:
+                parentToChildMap[parent].append(child)
+            else:
+                parentToChildMap[parent] = [child]
+
+    return (childToParentMap, parentToChildMap)
+
+
+def compute_neighbor_docs(query_distances, query_neighbors, index, docList, docsToClasses, embeddedDocText):
+    classesToNeighbors = {}
+    
+    for docStringIndex, embeddedDocStringNeighbors in enumerate(query_neighbors):
+        docString = docList[ docStringIndex ]
+
+        allNeighborClasses = set()
+        for neighborDocStringIndex in embeddedDocStringNeighbors:
+            neighborClasses = docsToClasses[ docList[ neighborDocStringIndex ] ]
+            for neighborClass in neighborClasses:
+                allNeighborClasses.add(neighborClass)
+
+        for klass in docsToClasses[ docList[docStringIndex] ]:
+            classesToNeighbors[ klass ] = allNeighborClasses
+
+    return classesToNeighbors
+ 
+
+def compareOverlap(mapTuple, staticMap):
+    childToParentMap = mapTuple[0]
+    parentToChildMap = mapTuple[1]
+    overlapPercents = []
+    for targetClass, relatedClasses in staticMap.items(): 
+        if len(relatedClasses) > 10:
+            continue
+        overlapNumerator = 0
+        overlapDenominator = 0
+        overlapDenominator = len(relatedClasses)
+        classParents = []
+        if targetClass in childToParentMap:
+            classParents = childToParentMap[targetClass]
+        classChildren = []
+        if targetClass in parentToChildMap:
+            classChildren = parentToChildMap[targetClass]
+        classSiblingLL = []
+        for parent in classParents:
+            if parent in parentToChildMap:
+                classSiblingLL.append(parentToChildMap[parent])
+        for usageClass in relatedClasses:
+            if usageClass in classParents:
+                overlapNumerator += 1
+            elif usageClass in classChildren:
+                overlapNumerator += 1
+            else:
+                for siblingList in classSiblingLL:
+                    if usageClass in siblingList:
+                        overlapNumerator += 1
+                        break
+        # below are some optional metrics that you can uncomment to enable
+        '''print("For target class", targetClass, "we have these parents:", classParents)
+        print("And these children:", classChildren)
+        print("And these siblings:", classSiblingLL)
+        print("And these related classes by usage", relatedClasses)
+        print("And results of overlap comparison are: " + str(overlapNumerator) + \
+        "/" + str(overlapDenominator))
+        input()'''
+        overlapPercent = overlapNumerator/overlapDenominator
+        overlapPercents.append(overlapPercent)
+    print("Average overlap is", sum(overlapPercents)/len(overlapPercents))
+
+def evaluate_static_analysis(classesToDocstringNeighbors, usagePath):
+    with open(usagePath, 'r') as staticData:
+        maxCount = 0
+        maxId = 0
+        ids = {}
+        predicted = {}
+        expected = {}
+        matchString = '(.+) (\d+) \[(.+)\]'
+        for line in staticData:
+            pattern = re.compile(matchString)
+            adjustedLine = pattern.match(line)
+            if adjustedLine == None:
+                print("Found violation.")
+                print(line)
+            count = int(adjustedLine.group(2))
+            if count > maxCount:
+                maxCount = count
+                
+            thisone = adjustedLine.group(1)
+            if thisone in classesToDocstringNeighbors:
+                myneighbors = classesToDocstringNeighbors[ thisone ]
+
+                overlap = 0
+                outside = 0
+                otherClasses = adjustedLine.group(3).strip().split(', ')
+                for otherClass in otherClasses:
+                    if otherClass in myneighbors:
+                        overlap = overlap + 1
+                    else:
+                        outside = outside + 1
+
+                print(str(thisone) + " " + str(overlap) + " " + str(outside) + " " + str(count))
+
+                expectedIds = []
+                for n in myneighbors:
+                    if n in ids:
+                        expectedIds.append(ids[n])
+                    else:
+                        ids[n] = maxId
+                        expectedIds.append(maxId)
+                        maxId = maxId + 1
+                        
+                if count in expected:
+                    expected[count].append(np.array(expectedIds))
+                else:
+                    expected[count] = [np.array(expectedIds)]
+                    
+                predictedIds = []
+                for n in otherClasses:
+                    if n in ids:
+                        predictedIds.append(ids[n])
+                    else:
+                        ids[n] = maxId
+                        predictedIds.append(maxId)
+                        maxId = maxId + 1
+
+                if count in predicted:
+                    predicted[count].append(np.array(predictedIds))
+                else:
+                    predicted[count] = [np.array(predictedIds)]
+
+    countExpected = []
+    countPredicted = []
+    for i in range(maxCount, 0, -1):
+        if i in predicted:
+            countExpected.extend(expected[i])
+            countPredicted.extend(predicted[i])
+            print(str(i) + ": " + str(ranking_metrics.mrr(countExpected, countPredicted)))
+
+            
+if __name__ == '__main__':
+    hierarchyPath = sys.argv[1]
+    docPath = sys.argv[2]
+    classPath = sys.argv[3]
+    usagePath = sys.argv[4]
+#    hierarchyMaps = build_sibling_maps(hierarchyPath)
+    (index, docList, docsToClasses, embeddedDocText, classesToDocs) = build_index_docs(docPath)
+    top_k = 50
+    query_distances, query_neighbors = index.search(embeddedDocText, top_k)
+    classesToDocstringNeighbors = compute_neighbor_docs(query_distances, query_neighbors, index, docList, docsToClasses, embeddedDocText)
+#    classesToUsageNeighbors = build_static_map(usagePath)
+#    compareOverlap(hierarchyMaps, staticAnalysis)
+#    classMap = build_class_mapping(classPath)
+    evaluate_static_analysis(classesToDocstringNeighbors, usagePath);
+
