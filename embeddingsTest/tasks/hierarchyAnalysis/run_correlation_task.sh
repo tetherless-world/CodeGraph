47b6f8ec847888557fbed2a5ff3c2a600e75c29e
Kavitha Srinivas <kavitha.srinivas@ibm.com>
2020-09-15 18:41:06 +0000

    hierarchy analysis working on test_data

--- embeddingsTest/tasks/hierarchyAnalysis/run_correlation_task.sh
+++ embeddingsTest/tasks/hierarchyAnalysis/run_correlation_task.sh
@@ -4,4 +4,4 @@ nohup python correlation_eval.py --top_k 10 --class2superclass_file /data/class2
 
 nohup python correlation_eval.py --top_k 10 --class2superclass_file /data/class2superclasses.csv --docstrings_file /data/merge-15-22.2.format.json --classmap /data/classes.map --classfail /data/classes.fail --embed_type bert > corr_bert.results
 
-nohup python correlation_eval.py --top_k 10 --class2superclass_file /data/class2superclasses.csv --docstrings_file /data/embedding/merge-15-22.2.format.json --classmap /data/classes.map --classfail /data/classes.fail --embed_type roberta > corr_roberta.results
+nohup python correlation_eval.py --top_k 10 --class2superclass_file /data/class2superclasses.csv --docstrings_file /data/merge-15-22.2.format.json --classmap /data/classes.map --classfail /data/classes.fail --embed_type roberta > corr_roberta.results
