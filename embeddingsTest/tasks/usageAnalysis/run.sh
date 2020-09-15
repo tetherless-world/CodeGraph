250393e8aa97d61ab5722fd11e77fc7aa20ed5a1
Julian Dolby <dolby@localhost.localdomain>
2020-09-15 16:33:06 -0400

    work on tests

--- embeddingsTest/tasks/usageAnalysis/run.sh
+++ embeddingsTest/tasks/usageAnalysis/run.sh
@@ -5,20 +5,18 @@ DIR=`dirname $ME`
 
 DATA=$1
 
-cd $DATA
-
 bash $DIR/run_use.sh $DATA
 
 if [ -f /tmp/out_usage_bert ]; then
     echo "using /tmp/out_usage_bert"
 else
-    PYTHONPATH=.. python staticAnalysis2.py classes2superclass.out merge-15-22.2.format.json classes.map usage.txt bert >/tmp/out_usage_bert
+    PYTHONPATH=.. python staticAnalysis2.py $DATA/classes2superclass.out $DATA/merge-15-22.2.format.json $DATA/classes.map $DATA/usage.txt bert >/tmp/out_usage_bert
 fi
 
 if [ -f /tmp/out_usage_roberta ]; then
     echo "using /tmp/out_usage_roberta"
 else
-    PYTHONPATH=.. python staticAnalysis2.py classes2superclass.out merge-15-22.2.format.json classes.map usage.txt roberta >/tmp/out_usage_roberta
+    PYTHONPATH=.. python staticAnalysis2.py $DATA/classes2superclass.out $DATA/merge-15-22.2.format.json $DATA/classes.map $DATA/usage.txt roberta >/tmp/out_usage_roberta
 fi
 
 bash $DIR/plot.sh
