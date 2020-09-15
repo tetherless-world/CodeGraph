c89ef3dbc0ad324f4111c0b83a30ac632690c48a
Julian Dolby <dolby@localhost.localdomain>
2020-09-15 06:48:45 -0400

    split scripts

--- embeddingsTest/tasks/stackLinkedPosts/run.sh
+++ embeddingsTest/tasks/stackLinkedPosts/run.sh
@@ -1,38 +1,13 @@
 #!/bin/bash
 
-if [ -f stackoverflow_data_ranking.json ]; then
-    echo "using stackoverflow_data_ranking.json"
-else
-    wget https://archive.org/download/merge-15-22.2.format/stackoverflow_data_ranking.json.tar.gz
-    tar xzf stackoverflow_data_ranking.json.tar.gz
-fi
+DATA=$1
 
-if [ -f /tmp/dataset.json ]; then
-    echo "using /tmp/dataset.json"
-else
-    python dataset.py stackoverflow_data_ranking.json /tmp/dataset.json /tmp/testset.json
-fi
+bash dataset.py $DATA
 
-if [ -f /tmp/use_links.out ]; then
-    echo "using use_links.out"
-else
-    PYTHONPATH=.. python eval.py /tmp/dataset.json /tmp/testset.json use /tmp/true_use.json /tmp/false_use.json > /tmp/use_links.out 
-fi
+bash eval_use.sh
 
-if [ -f /tmp/bert_links.out ]; then
-    echo "using bert_links.out"
-else
-    PYTHONPATH=.. python eval.py /tmp/dataset.json /tmp/testset.json bert /tmp/true_bert.json /tmp/false_bert.json > /tmp/bert_links.out 
-fi
+bash eval_bert.sh
 
-if [ -f /tmp/roberta_links.out ]; then
-    echo "using roberta_links.out"
-else
-    PYTHONPATH=.. python eval.py /tmp/dataset.json /tmp/testset.json roberta /tmp/true_roberta.json /tmp/false_roberta.json > /tmp/roberta_links.out 
-fi
+bash eval_roberta.sh
 
-awk '/[0-9.]+/ { match($0, /([0-9.]+)/, a); print(a[1]); }' /tmp/true_use.json > true.dat
-
-awk '/[0-9.]+/ { match($0, /([0-9.]+)/, a); print(a[1]); }' /tmp/false_use.json > false.dat
-
-gnuplot distances.gnuplot > links.pdf
+bash plot.sh
--- embeddingsTest/tasks/usageAnalysis/run.sh
+++ embeddingsTest/tasks/usageAnalysis/run.sh
@@ -3,35 +3,11 @@
 ME=$0
 DIR=`dirname $ME`
 
-if [[ -f classes2superclass.out ]]; then
-    echo "using classes2superclass.out"
-else
-    wget https://archive.org/download/merge-15-22.2.format/classes2superclass.out
-fi
+DATA=$1
 
-if [[ -f merge-15-22.2.format.json ]]; then
-    echo "using merge-15-22.2.format.json"
-else
-    wget https://archive.org/download/merge-15-22.2.format/merge-15-22.2.format.json
-fi
+cd $DATA
 
-if [[ -f classes.map ]]; then
-    echo "using classes.map"
-else
-    wget https://archive.org/download/merge-15-22.2.format/classes.map
-fi
-
-if [[ -f usage.txt ]]; then
-    echo "using usage.txt"
-else
-    wget https://archive.org/download/merge-15-22.2.format/usage.txt
-fi
-
-if [ -f /tmp/out_usage_USE ]; then
-    echo "using /tmp/out_usage_USE"
-else
-    PYTHONPATH=.. python staticAnalysis2.py classes2superclass.out merge-15-22.2.format.json classes.map usage.txt USE >/tmp/out_usage_USE
-fi
+bash $DIR/run_use.sh $DATA
 
 if [ -f /tmp/out_usage_bert ]; then
     echo "using /tmp/out_usage_bert"
@@ -45,8 +21,4 @@ else
     PYTHONPATH=.. python staticAnalysis2.py classes2superclass.out merge-15-22.2.format.json classes.map usage.txt roberta >/tmp/out_usage_roberta
 fi
 
-awk -f mrr-map.awk -v start=103 /tmp/out_usage_USE > mrr-map-use.dat
-awk -f mrr-map.awk -v start=103 /tmp/out_usage_bert > mrr-map-bert.dat
-awk -f mrr-map.awk -v start=103 /tmp/out_usage_roberta > mrr-map-roberta.dat
-
-gnuplot mrr-map.gnuplot > mrr-map.pdf
+bash $DIR/plot.sh
