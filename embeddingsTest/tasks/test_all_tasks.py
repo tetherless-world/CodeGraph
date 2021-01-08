df0d9650323a6dcecfd30cc923daf4aa3fec9604
Kavitha Srinivas <Kavitha.Srinivas@ibm.com>
2021-01-08 13:50:22 -0500

    fixing minor errors

--- embeddingsTest/tasks/test_all_tasks.py
+++ embeddingsTest/tasks/test_all_tasks.py
@@ -23,15 +23,15 @@ if __name__ == '__main__':
         model = util.get_model(args.embed_type, local_model_path=args.model_dir)
     else:
         model = util.get_model(args.embed_type)
-        
-    test_hierarchy_samples = create_hirerachy_examples(os.join(data_dir, 'hierarchy_test.json'))
-    test_linked_posts = create_linked_posts(os.join(data_dir, 'stackoverflow_data_linkedposts__testing.json'))
-    test_class_posts = create_train_class_posts(os.join(data_dir, 'class_posts_test_data.json'))
-    test_usage = create_train_usage(os.join(data_dir, 'usage_test.json'))
-    test_posts_ranking = create_posts_ranking(os.join(data_dir, 'stackoverflow_data_ranking_v2_testing.json'))
-    test_search = create_search(os.join(data_dir, 'stackoverflow_matches_codesearchnet_5k_test_collection.tsv'),
-                                 os.join(data_dir, 'stackoverflow_matches_codesearchnet_5k_test_queries.tsv'),
-                                 os.join(data_dir, 'stackoverflow_matches_codesearchnet_5k_test_blanca-qidpidtriples.train.tsv'))
+
+    test_hierarchy_samples = create_hirerachy_examples(os.path.join(data_dir, 'hierarchy_test.json'))
+    test_linked_posts = create_linked_posts(os.path.join(data_dir, 'stackoverflow_data_linkedposts__testing.json'))
+    test_class_posts = create_train_class_posts(os.path.join(data_dir, 'class_posts_test_data.json'))
+    test_usage = create_train_usage(os.path.join(data_dir, 'usage_test.json'))
+    test_posts_ranking = create_posts_ranking(os.path.join(data_dir, 'stackoverflow_data_ranking_v2_testing.json'))
+    test_search = create_search(os.path.join(data_dir, 'stackoverflow_matches_codesearchnet_5k_test_collection.tsv'),
+                                 os.path.join(data_dir, 'stackoverflow_matches_codesearchnet_5k_test_queries.tsv'),
+                                 os.path.join(data_dir, 'stackoverflow_matches_codesearchnet_5k_test_blanca-qidpidtriples.train.tsv'))
 
 
     test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_hierarchy_samples, name='test-hierarchy-samples')
