b7187d425084982ff6c0253587f658a871d18451
Kavitha Srinivas <Kavitha.Srinivas@ibm.com>
2021-01-08 15:55:08 -0500

    hierarchyAnalysis/hierarchy_stats.py

--- embeddingsTest/tasks/hierarchyAnalysis/hierarchy_stats.py
+++ embeddingsTest/tasks/hierarchyAnalysis/hierarchy_stats.py
@@ -23,16 +23,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Hierarchy prediction based on embeddings')
     parser.add_argument('--eval_file', type=str,
                         help='train/test file')
-    parser.add_argument('--class2superclass_file', type=str,
-                        help='csv of classes to superclasses')
     parser.add_argument('--docstrings_file', type=str,
                         help='docstrings for classes, functions etc')
-    parser.add_argument('--classmap', type=str,
-                        help='classes to real class names as determined by dynamic loading of class')
-    parser.add_argument('--classfail', type=str,
-                        help='classes that fail to load to determine real class mappings')
     parser.add_argument('--embed_type', type=str,
-                        help='USE or bert or roberta')
+                        help='USE or bert or roberta or finetuned or bertoverflow')
+    parser.add_argument('--model_dir',type=str,
+                        help='dir for finetuned or bertoverflow models')
 
     args = parser.parse_args()
 
@@ -40,4 +36,4 @@ if __name__ == '__main__':
     print('evaluation')
 
     with open(args.eval_file) as f:
-        util.evaluate_regression(f, args.docstrings_file, args.embed_type)
+        util.evaluate_regression(f, args.docstrings_file, args.embed_type, args.model_dir)
