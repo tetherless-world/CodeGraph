3e54d2530ff6c4010ec650ef3795958891d5c878
Kavitha Srinivas <Kavitha.Srinivas@ibm.com>
2021-01-11 16:00:52 -0500

    bug

--- embeddingsTest/tasks/test_all_tasks.py
+++ embeddingsTest/tasks/test_all_tasks.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     test_linked_posts = create_linked_posts('stackoverflow_data_linkedposts__testing.json', data_dir, model, validate=None, is_test=True)
     test_class_posts = create_train_class_posts('class_posts_test_data.json', data_dir, model, validate=None, is_test=True)
     test_usage = create_train_usage('usage_test.json', data_dir, model, validate=None, is_test=True)
-    test_posts_ranking = create_posts_ranking('stackoverflow_data_ranking_v2_testing.json', data_dir, model)
+    test_posts_ranking = create_posts_ranking('stackoverflow_data_ranking_v2_testing.json', data_dir, model, validate=None, is_test=True)
     test_search = create_search('stackoverflow_matches_codesearchnet_5k_test_collection.tsv',
                                  'stackoverflow_matches_codesearchnet_5k_test_queries.tsv',
                                  'stackoverflow_matches_codesearchnet_5k_test_blanca-qidpidtriples.train.tsv', data_dir,
