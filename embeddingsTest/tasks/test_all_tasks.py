9082339147541fd0cae3b122c4589a11f86d2869
Kavitha Srinivas <Kavitha.Srinivas@ibm.com>
2021-01-11 14:43:26 -0500

    adding testing

--- embeddingsTest/tasks/test_all_tasks.py
+++ embeddingsTest/tasks/test_all_tasks.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     test_hierarchy_samples = create_hirerachy_examples('hierarchy_test.json', data_dir)
     test_linked_posts = create_linked_posts('stackoverflow_data_linkedposts__testing.json', data_dir)
     test_class_posts = create_train_class_posts('class_posts_test_data.json', data_dir)
-    test_usage = create_train_usage(os.path.join('usage_test.json', data_dir)
+    test_usage = create_train_usage('usage_test.json', data_dir)
     test_posts_ranking = create_posts_ranking('stackoverflow_data_ranking_v2_testing.json', data_dir)
     test_search = create_search('stackoverflow_matches_codesearchnet_5k_test_collection.tsv',
                                  'stackoverflow_matches_codesearchnet_5k_test_queries.tsv',
