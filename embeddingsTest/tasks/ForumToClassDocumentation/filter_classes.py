d4f66779736cb9078b0ff867d946fa29dc617a29
Ibrahim Abdelaziz <i.hosny2@gmail.com>
2021-04-25 11:21:28 -0400

    forumClass task -- regenerate task data

--- embeddingsTest/tasks/ForumToClassDocumentation/filter_classes.py
+++ embeddingsTest/tasks/ForumToClassDocumentation/filter_classes.py
@@ -19,11 +19,15 @@ def read_into_results(ids, id2hits, docs, class2class, label):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("--positive_file", help="Files to get positives", required=True)
-    parser.add_argument("--negative_file", help="Files to get negatives", required=True)
-    parser.add_argument("--all_docstrings_file", help="Files to get docstrings", required=True)
-    parser.add_argument("--class_map", help="class map", required=True)
+    parser.add_argument("--positive_file", help="Files to get positives")
+    parser.add_argument("--negative_file", help="Files to get negatives")
+    parser.add_argument("--all_docstrings_file", help="Files to get docstrings")
+    parser.add_argument("--class_map", help="class map")
 
+    # parser.set_defaults(positive_file = '../test_data/class_matches_in_stackoverflow.json',
+    #                     negative_file = '../test_data/class_matches_in_stackoverflow.json',
+    #                     all_docstrings_file = '../../../code_knowledge_graph/data/full_docstrings-merge-15-22/all/merge-15-22.2.format.json',
+    #                     class_map = '../test_data/classes.map')
     args = parser.parse_args()
 
     with open(args.positive_file) as f:
@@ -63,6 +67,8 @@ if __name__ == "__main__":
     with open(args.all_docstrings_file) as f:
         data = json.load(f)
         for obj in data:
+            if 'klass' not in obj or 'class_docstring' not in obj:
+                continue
             klass = obj['klass']
             if klass not in class2class:
                 continue
@@ -81,22 +87,52 @@ if __name__ == "__main__":
     
     ids = {}
     neg_ids2hits = {}
-    
+    num_samples = 0
     for hit in data:
         for clazz in hit['relevant_class_alias'][0]:
+            if clazz in ids and len(ids[clazz])>10:
+                print(f'clazz has more than 10 hits, continue')
+                continue
             pkg = ' ' + clazz.split('.')[0]
             clazz_part = clazz.split('.')[-1]
-            if pkg not in hit['title'] and clazz_part not in hit['title'] and hit['id'] not in ids2hits and hit['id'] not in neg_ids2hits:
+            # if pkg not in hit['title'] and clazz_part not in hit['title'] and hit['id'] not in ids2hits and hit['id'] not in neg_ids2hits:
+            if clazz_part not in hit['title'] and clazz_part not in hit['text'] and hit['id'] not in ids2hits and hit['id'] not in neg_ids2hits:
                 neg_ids2hits[hit['id']] = hit
                 if clazz not in ids:
                     ids[clazz] = set()
                 ids[clazz].add(hit['id'])
+                num_samples += 1
+            if num_samples > 10000:
+                print(f'Found {num_samples} samples, break')
+                break
+        if num_samples > 10000:
+            print(f'Found {num_samples} samples, break')
+            break
 
     results = read_into_results(ids, neg_ids2hits, class2docstring, class2class, 0)
     assert len(set(ids2hits.keys()).intersection(set(neg_ids2hits.keys()))) == 0
-    with open('/tmp/class_matches_stackoverflow_negatives.json', 'w') as f:
+    with open('/tmp/class_matches_stackoverflow_negatives_v3.json', 'w') as f:
         json.dump(results, f, indent=4)
     
     print('number of negative classes:' + str(len(results.keys())))
 
-    
+''''
+v1:             # if pkg not in hit['title'] and clazz_part not in hit['title'] and hit['id'] not in ids2hits and hit['id'] not in neg_ids2hits:
+hits:3685
+number of positive classes:1753
+hits:18010
+number of negative classes:4881
+
+
+
+
+v2:             if pkg not in hit['title'] and pkg not in hit['text'] and clazz_part not in hit['title'] and clazz_part not in hit['text'] and hit['id'] not in ids2hits and hit['id'] not in neg_ids2hits:
+
+python filter_classes.py --positive_file /data/blanca/class_matches_in_stackoverflow_v5.json --negative_file /data/blanca/class_matches_in_stackoverflow_v4_1m_shuffled.json --all_docstrings_file /home/ibrahim/full_docstrings-merge-15-22/all/merge-15-22.2.format.json --class_map ../test_data/classes.map
+number of positive classes:2261
+Found 10001 samples, break
+Found 10001 samples, break
+hits:5003
+number of negative classes:4685
+
+'''
